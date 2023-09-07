8aca59d88aeac232cefc881079266d92451a28d4
Austin Andrews <admin@templarian.com>
2023-09-07 16:14:41 -0500

    Toolbar Preview

--- src/App.tsx
+++ src/App.tsx
@@ -615,23 +615,32 @@ function App() {
               <h2>Preview</h2>
               <h3>Interface</h3>
               <div className="flyout-padding">
-                <div>
-                  <div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: 1 }}>
+                    <h4>Round Button</h4>
                     <svg className="example-button-round" viewBox="0 0 146 36">
                       <g transform="translate(7,7)">
                         <path fill="currentColor" d={path} />
                       </g>
                       <path fill="currentColor" d="M14 4H140V5H141V6H142V30H141V31H140V32H14V31H12V30H10V29H9V28H8V27H7V26H6V24H5V22H4V14H5V12H6V10H7V9H8V8H9V7H10V6H12V5H14V4M8 23V25H9V26H10V27H11V28H13V29H15V30H139V29H140V7H139V6H15V7H13V8H11V9H10V10H9V11H8V13H7V15H6V21H7V23H8M32 10H34V16H40V10H42V24H40V18H34V24H32V10M55 14H57V15H58V14H63V15H64V24H62V17H61V16H58V17H57V24H55V14M73 10H75V24H73V23H72V24H68V23H67V22H66V16H67V15H68V14H72V15H73V10M68 21H69V22H72V21H73V17H72V16H69V17H68V21M77 15H78V14H83V15H84V14H86V26H85V27H84V28H78V26H83V25H84V24H79V23H78V22H77V15M84 17H83V16H79V21H80V22H83V21H84V17M91 10V22H92V24H90V23H89V12H88V10H91M96 14H102V15H103V16H104V22H103V23H102V24H96V23H95V22H94V16H95V15H96V14M96 21H97V22H101V21H102V17H101V16H97V17H96V21M105 14H107V16H108V18H109V20H111V18H112V16H113V14H115V17H114V19H113V21H112V23H111V24H109V23H108V21H107V19H106V17H105V14M118 14H125V15H126V19H125V20H119V21H120V22H125V24H119V23H118V22H117V15H118V14M124 18V16H119V18H124M128 15H129V14H135V15H136V17H134V16H130V17H131V18H133V19H135V20H136V23H135V24H129V23H128V21H130V22H134V21H133V20H131V19H129V18H128V15M45 14H51V15H52V16H53V24H51V23H50V24H45V23H44V19H45V18H51V17H50V16H46V17H44V15H45V14M50 22V21H51V20H46V22H50Z" />
                     </svg>
-                    <svg className="example-button-round" viewBox="0 0 146 36">
-                      <g transform="translate(7,7)">
-                        <path fill="currentColor" d={path} />
-                      </g>
-                      <path fill="currentColor" d="M6 4H140V5H141V6H142V30H141V31H140V32H6V31H5V30H4V6H5V5H6V4M6 29H7V30H139V29H140V7H139V6H7V7H6V29M32 10H34V16H40V10H42V24H40V18H34V24H32V10M55 14H57V15H58V14H63V15H64V24H62V17H61V16H58V17H57V24H55V14M73 10H75V24H73V23H72V24H68V23H67V22H66V16H67V15H68V14H72V15H73V10M68 21H69V22H72V21H73V17H72V16H69V17H68V21M77 15H78V14H83V15H84V14H86V26H85V27H84V28H78V26H83V25H84V24H79V23H78V22H77V15M84 17H83V16H79V21H80V22H83V21H84V17M91 10V22H92V24H90V23H89V12H88V10H91M96 14H102V15H103V16H104V22H103V23H102V24H96V23H95V22H94V16H95V15H96V14M96 21H97V22H101V21H102V17H101V16H97V17H96V21M105 14H107V16H108V18H109V20H111V18H112V16H113V14H115V17H114V19H113V21H112V23H111V24H109V23H108V21H107V19H106V17H105V14M118 14H125V15H126V19H125V20H119V21H120V22H125V24H119V23H118V22H117V15H118V14M124 18V16H119V18H124M128 15H129V14H135V15H136V17H134V16H130V17H131V18H133V19H135V20H136V23H135V24H129V23H128V21H130V22H134V21H133V20H131V19H129V18H128V15M45 14H51V15H52V16H53V24H51V23H50V24H45V23H44V19H45V18H51V17H50V16H46V17H44V15H45V14M50 22V21H51V20H46V22H50Z" />
-                    </svg>
                   </div>
-                  <div>
-                  <svg className="example-button-round" viewBox="0 0 146 36">
+                  <div style={{ display: 'flex' }}>
+                    <div style={{ flex: 1 }}>
+                      <h4>Square Button</h4>
+                      <svg className="example-button-round" viewBox="0 0 146 36">
+                        <g transform="translate(7,7)">
+                          <path fill="currentColor" d={path} />
+                        </g>
+                        <path fill="currentColor" d="M6 4H140V5H141V6H142V30H141V31H140V32H6V31H5V30H4V6H5V5H6V4M6 29H7V30H139V29H140V7H139V6H7V7H6V29M32 10H34V16H40V10H42V24H40V18H34V24H32V10M55 14H57V15H58V14H63V15H64V24H62V17H61V16H58V17H57V24H55V14M73 10H75V24H73V23H72V24H68V23H67V22H66V16H67V15H68V14H72V15H73V10M68 21H69V22H72V21H73V17H72V16H69V17H68V21M77 15H78V14H83V15H84V14H86V26H85V27H84V28H78V26H83V25H84V24H79V23H78V22H77V15M84 17H83V16H79V21H80V22H83V21H84V17M91 10V22H92V24H90V23H89V12H88V10H91M96 14H102V15H103V16H104V22H103V23H102V24H96V23H95V22H94V16H95V15H96V14M96 21H97V22H101V21H102V17H101V16H97V17H96V21M105 14H107V16H108V18H109V20H111V18H112V16H113V14H115V17H114V19H113V21H112V23H111V24H109V23H108V21H107V19H106V17H105V14M118 14H125V15H126V19H125V20H119V21H120V22H125V24H119V23H118V22H117V15H118V14M124 18V16H119V18H124M128 15H129V14H135V15H136V17H134V16H130V17H131V18H133V19H135V20H136V23H135V24H129V23H128V21H130V22H134V21H133V20H131V19H129V18H128V15M45 14H51V15H52V16H53V24H51V23H50V24H45V23H44V19H45V18H51V17H50V16H46V17H44V15H45V14M50 22V21H51V20H46V22H50Z" />
+                      </svg>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: 1 }}>
+                    <h4>Toolbar</h4>
+                    <svg className="example-button-round" viewBox="0 0 146 36">
                       <g transform="translate(7,7)">
                         <path fill="currentColor" d="M12,12H10V6H12ZM12,16H10V14H12ZM15,21H7V20H5V19H4V18H3V17H2V15H1V7H2V5H3V4H4V3H5V2H7V1H15V2H17V3H18V4H19V5H20V7H21V15H20V17H19V18H18V19H17V20H15ZM14,19V18H16V17H17V16H18V14H19V8H18V6H17V5H16V4H14V3H8V4H6V5H5V6H4V8H3V14H4V16H5V17H6V18H8V19Z" />
                       </g>
@@ -647,90 +656,90 @@ function App() {
                       <path fill="currentColor" d="M6 4H140V5H141V6H142V30H141V31H140V32H6V31H5V30H4V6H5V5H6V4M7 29V30H30V26H31V30H55V26H56V30H80V26H81V30H105V26H106V30H139V29H140V24H139V17H138V16H135V17H134V24H132V14H134V15H135V14H140V7H139V6H106V10H105V6H81V10H80V6H56V10H55V6H31V10H30V6H7V7H6V29H7M109 10H111V16H117V10H119V24H117V18H111V24H109V10M122 14H128V15H129V16H130V24H128V23H127V24H122V23H121V19H122V18H128V17H127V16H123V17H121V15H122V14M127 22V21H128V20H123V22H127Z" />
                     </svg>
                   </div>
-                  <div style={{ display: 'flex' }}>
-                    <div style={{ flex: 1 }}>
-                      <h4>Grid Preview</h4>
-                      <svg className="example-grid" viewBox="0 0 130 82">
-                        <g transform="translate(6,6)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(30,6)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(54,6)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(78,6)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(102,6)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(6,30)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(30,30)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(54,30)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(78,30)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(102,30)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(6,54)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(30,54)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(54,54)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(78,54)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(102,54)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <path fill="#AAA" d="M4 4H8V6H6V8H4V4M26 4H32V6H30V8H28V6H26V4M50 4H56V6H54V8H52V6H50V4M74 4H80V6H78V8H76V6H74V4M4 78V74H6V76H8V78H4M98 4H104V6H102V8H100V6H98V4M32 78H26V76H28V74H30V76H32V78M56 78H50V76H52V74H54V76H56V78M80 78H74V76H76V74H78V76H80V78M104 78H98V76H100V74H102V76H104V78M126 78H122V76H124V74H126V78M126 4V8H124V6H122V4H126M126 50V56H124V54H122V52H124V50H126M126 26V32H124V30H122V28H124V26H126M4 32V26H6V28H8V30H6V32H4M4 56V50H6V52H8V54H6V56H4M28 32V30H26V28H28V26H30V28H32V30H30V32H28M52 32V30H50V28H52V26H54V28H56V30H54V32H52M76 32V30H74V28H76V26H78V28H80V30H78V32H76M100 32V30H98V28H100V26H102V28H104V30H102V32H100M28 56V54H26V52H28V50H30V52H32V54H30V56H28M52 56V54H50V52H52V50H54V52H56V54H54V56H52M76 56V54H74V52H76V50H78V52H80V54H78V56H76M100 56V54H98V52H100V50H102V52H104V54H102V56H100Z" />
-                      </svg>
-                    </div>
-                    <div style={{ flex: 1 }}>
-                      <h4>Tile Preview</h4>
-                      <svg className="example-tile" viewBox="0 0 66 66">
-                        <g transform="translate(0, 0)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(22, 0)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(44, 0)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(0, 22)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(22, 22)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(44, 22)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(0, 44)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(22, 44)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                        <g transform="translate(44, 44)">
-                          <path fill="currentColor" d={path} />
-                        </g>
-                      </svg>
-                    </div>
+                </div>
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: 1 }}>
+                    <h4>Grid</h4>
+                    <svg className="example-grid" viewBox="0 0 130 82">
+                      <g transform="translate(6,6)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(30,6)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(54,6)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(78,6)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(102,6)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(6,30)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(30,30)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(54,30)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(78,30)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(102,30)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(6,54)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(30,54)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(54,54)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(78,54)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(102,54)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <path fill="#AAA" d="M4 4H8V6H6V8H4V4M26 4H32V6H30V8H28V6H26V4M50 4H56V6H54V8H52V6H50V4M74 4H80V6H78V8H76V6H74V4M4 78V74H6V76H8V78H4M98 4H104V6H102V8H100V6H98V4M32 78H26V76H28V74H30V76H32V78M56 78H50V76H52V74H54V76H56V78M80 78H74V76H76V74H78V76H80V78M104 78H98V76H100V74H102V76H104V78M126 78H122V76H124V74H126V78M126 4V8H124V6H122V4H126M126 50V56H124V54H122V52H124V50H126M126 26V32H124V30H122V28H124V26H126M4 32V26H6V28H8V30H6V32H4M4 56V50H6V52H8V54H6V56H4M28 32V30H26V28H28V26H30V28H32V30H30V32H28M52 32V30H50V28H52V26H54V28H56V30H54V32H52M76 32V30H74V28H76V26H78V28H80V30H78V32H76M100 32V30H98V28H100V26H102V28H104V30H102V32H100M28 56V54H26V52H28V50H30V52H32V54H30V56H28M52 56V54H50V52H52V50H54V52H56V54H54V56H52M76 56V54H74V52H76V50H78V52H80V54H78V56H76M100 56V54H98V52H100V50H102V52H104V54H102V56H100Z" />
+                    </svg>
+                  </div>
+                  <div style={{ flex: 1 }}>
+                    <h4>Repeating Tile</h4>
+                    <svg className="example-tile" viewBox="0 0 66 66">
+                      <g transform="translate(0, 0)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(22, 0)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(44, 0)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(0, 22)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(22, 22)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(44, 22)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(0, 44)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(22, 44)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                      <g transform="translate(44, 44)">
+                        <path fill="currentColor" d={path} />
+                      </g>
+                    </svg>
                   </div>
                 </div>
               </div>
